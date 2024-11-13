8f74086f31752104bf8335d9cbc111f94c987010
anshuman008 <code.anshu@gmail.com>
2024-11-13 21:05:03 +0530

    routing added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+"use client"
+import { UserButton, useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react'
 
 const page = () => {
+
+   const {user} = useUser();
+   const router = useRouter();
+
+
+   useEffect(() => {
+    if(user?.primaryEmailAddress?.emailAddress){
+      router.push("/admin")
+    };
+
+   },[user]);
+
+
   return (
     <div>
        <h1>Hello there!!</h1>
